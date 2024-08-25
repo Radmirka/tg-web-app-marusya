68df2d2c579319c9a99c0ea41a7d91e2319d88ff
unknown <yafarov9119@gmail.com>
2024-08-25 20:21:03 +0300

    Form and Productlist v1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './components/hooks/useTelegram';
 import Header from './components/Header/Header';
+import {Route, Routes} from 'react-router-dom';
+import ProductList from './components/PorductList/ProductList';
+import Form from './components/Form/Form';
 
 
 function App() {
@@ -14,7 +17,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={<ProductList />}/>
+        <Route path={'form'} element={<Form />}/>
+      </Routes>
     </div>
   );
 }
